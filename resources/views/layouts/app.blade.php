5dfbc570d39927d755e0164e9e802756ffd97d6f
Samuel <sambarbosa66@gmail.com>
2025-04-30 19:53:42 -0500

    Vigesimosegun commit: Cosas pequeñas pero rapidas 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-    <link rel="icon" type="image/jpeg" href="{{ asset('img/favicon.jpg') }}">
+    <link rel="icon" type="image/jpeg" href="{{ asset('img/favicon.ico') }}">
 
 
     <!-- Styles / Scripts -->
